d168fa6d4805a948ffc90ac7396cd5f06c28fcf5
jvittor <seu@email.com>
2024-01-27 11:22:16 -0300

    :bug: fix: ajustando o input card

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -18,7 +18,6 @@ import {
   ModalOverlay,
   ModalContent,
   ModalHeader,
-  ModalCloseButton,
   ModalBody,
 } from "@chakra-ui/react";
 import LogoOrange from "../assets/logo_orange.png";
@@ -135,101 +134,207 @@ const Home = () => {
         </Link>
       </Button>
 
-      <Card
-        position="absolute"
-        top="400px"
-        left="60px"
-        width="389px"
-        height="258px"
-        borderRadius="4px"
-        bg="#E6E9F2"
-        cursor="pointer"
-        onClick={openUploadProjectModal}
-      >
-        <Flex
-          direction="column"
-          justify="center"
-          align="center"
-          height="100%"
+      <Link>
+        <Card
+          position="absolute"
+          top="400px"
+          left="60px"
+          width="389px"
+          height="258px"
+          borderRadius="4px"
+          bg="#E6E9F2"
+          cursor="pointer"
+          onClick={openUploadProjectModal}
+          _hover={{ bg: "#C2C4CC" }}
         >
-          <Image src={UploadProject} alt="UploadProject" mb={5} />
-          <Text
-            color="#303133"
-            fontFamily="Roboto"
-            fontSize="16px"
-            fontStyle="normal"
-            fontWeight="400"
-            lineHeight="16px"
-            letterSpacing="0.5px"
-            opacity="0.5"
+          <Flex
+            direction="column"
+            justify="center"
+            align="center"
+            height="100%"
           >
-            Adicione seu primeiro projeto
-          </Text>
-          <Flex>
+            <Image src={UploadProject} alt="UploadProject" mb={5} />
             <Text
               color="#303133"
               fontFamily="Roboto"
-              fontSize="14px"
+              fontSize="16px"
               fontStyle="normal"
               fontWeight="400"
               lineHeight="16px"
               letterSpacing="0.5px"
               opacity="0.5"
-              textAlign="right"
-              marginTop="22px"
             >
-              Compartilhe seu talento com milhares de pessoas
+              Adicione seu primeiro projeto
             </Text>
+            <Flex>
+              <Text
+                color="#303133"
+                fontFamily="Roboto"
+                fontSize="14px"
+                fontStyle="normal"
+                fontWeight="400"
+                lineHeight="16px"
+                letterSpacing="0.5px"
+                opacity="0.5"
+                textAlign="right"
+                marginTop="22px"
+              >
+                Compartilhe seu talento com milhares de pessoas
+              </Text>
+            </Flex>
           </Flex>
-        </Flex>
-      </Card>
+        </Card>
+      </Link>
 
 
 
 
 
       {/* Modal para upload de arquivinhos no botão "Adicionar Arquivos" perto do perfil*/}
-      <Modal isOpen={isAddProjectModalOpen} onClose={closeAddProjectModal}>
+      <Modal 
+      isOpen={isAddProjectModalOpen} 
+      onClose={closeAddProjectModal}>
         <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Adicionar Projeto</ModalHeader>
-          <ModalBody>
-            <Text mt={-2}>Selecione o conteúdo que você deseja fazer upload.</Text>
-            <Button colorScheme="teal" onClick={closeAddProjectModal} bg="#F52">
-              Salvar
-            </Button>
-            <Button 
-            ml={3} 
-            onClick={closeAddProjectModal}
-            color="rgba(0, 0, 0, 0.38)"
-            bordeRadius="4px"
-            background= "rgba(0, 0, 0, 0.12)"
-            >Cancelar</Button>
+        <ModalContent top={10} width="890px" height="510px" maxW="100%">
+          <ModalHeader
+          color="#515255"
+          fontFamily= "Roboto"
+          fontSize= "24px"
+          fontStyle= "normal"
+          fontWeight= "400"
+          lineHeight= "24px"
+          >Adicionar Projeto</ModalHeader>
+          <ModalBody width="100%">
+            <Text mt={-2}>
+              Selecione o conteúdo que você deseja fazer upload
+              <Card
+              display={"flex"}
+              bg="#E6E9F2"
+              width= {"389px"}
+              height= {"304px"}
+              justifyContent= {"center"}
+              alignItems= {"center"}
+              padding={"91px 59px 91px 60px"}
+              mt={3}
+              >
+                <Image src={UploadProject} alt="UploadProject" />
+                <Text 
+                mt={2}
+                color="#303133"
+                fontFamily= "Roboto"
+                fontSize= "14px"
+                fontStyle= "normal"
+                opacity={"0.6"}
+                >Compartilhe seu talento com milhares de pessoas</Text>
+              </Card>
+              </Text>
+              <Text mt={5}>Visualizar publicação</Text>
+              <Flex justify="space-between" display={"flex"} mt={4}>
+                <Button 
+                  colorScheme="teal" 
+                  onClick={closeAddProjectModal}
+                  bg="#F52"
+                  borderRadius="4px"
+                  padding="8px 27px"
+                  boxShadow="0px 3px 1px -2px rgba(0, 0, 0, 0.20), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12)"
+                  fontSize={"15px"}                
+                  fontFamily="Roboto"
+                  fontStyle={"normal"}
+                  lineHeight={"20px"}
+                >
+                  SALVAR
+                </Button>
+
+                <Button  
+                  onClick={closeAddProjectModal}
+                  color="rgba(0, 0, 0, 0.38)"
+                  background="rgba(0, 0, 0, 0.12)"
+                  borderRadius="4px"
+                  padding="8px 22px"
+                  boxShadow="0px 3px 1px -2px rgba(0, 0, 0, 0.20), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12)"
+                  right={590}
+                  fontSize={"15px"}
+                  fontFamily="Roboto"
+                  fontStyle={"normal"}
+                  lineHeight={"20px"}
+                >
+                  CANCELAR
+                </Button>
+              </Flex>
           </ModalBody>
         </ModalContent>
       </Modal>
-
       {/* Modal para upload de arquivinhos no Card*/}  
-      <Modal isOpen={isUploadProjectModalOpen} onClose={closeUploadProjectModal}>
+      <Modal 
+      isOpen={isUploadProjectModalOpen} 
+      onClose={closeUploadProjectModal}>
         <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Adicionar Projeto</ModalHeader>
-          <ModalBody>
-            <Text mt={-2}>Selecione o conteúdo que você deseja fazer upload</Text>
-            <Button 
-            colorScheme="teal" 
-            onClick={closeUploadProjectModal}
-            bg="#F52"
-            >
-              Salvar
-            </Button>
-            <Button 
-            ml={3} 
-            onClick={closeUploadProjectModal}
-            color="rgba(0, 0, 0, 0.38)"
-            bordeRadius="4px"
-            background= "rgba(0, 0, 0, 0.12)"
-            >Cancelar</Button>
+        <ModalContent top={10} width="890px" height="510px" maxW="100%">
+          <ModalHeader
+          color="#515255"
+          fontFamily= "Roboto"
+          fontSize= "24px"
+          fontStyle= "normal"
+          fontWeight= "400"
+          lineHeight= "24px"
+          >Adicionar Projeto</ModalHeader>
+          <ModalBody width="100%">
+            <Text mt={-2}>
+              Selecione o conteúdo que você deseja fazer upload
+              <Card
+              display={"flex"}
+              bg="#E6E9F2"
+              width= {"389px"}
+              height= {"304px"}
+              justifyContent= {"center"}
+              alignItems= {"center"}
+              padding={"91px 59px 91px 60px"}
+              mt={3}
+              >
+                <Image src={UploadProject} alt="UploadProject" />
+                <Text 
+                mt={2}
+                color="#303133"
+                fontFamily= "Roboto"
+                fontSize= "14px"
+                fontStyle= "normal"
+                opacity={"0.6"}
+                >Compartilhe seu talento com milhares de pessoas</Text>
+              </Card>
+              </Text>
+              <Text mt={5}>Visualizar publicação</Text>
+              <Flex justify="space-between" display={"flex"} mt={4}>
+                <Button 
+                  colorScheme="teal" 
+                  onClick={closeUploadProjectModal}
+                  bg="#F52"
+                  borderRadius="4px"
+                  padding="8px 27px"
+                  boxShadow="0px 3px 1px -2px rgba(0, 0, 0, 0.20), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12)"
+                  fontSize={"15px"}                
+                  fontFamily="Roboto"
+                  fontStyle={"normal"}
+                  lineHeight={"20px"}
+                >
+                  SALVAR
+                </Button>
+
+                <Button  
+                  onClick={closeUploadProjectModal}
+                  color="rgba(0, 0, 0, 0.38)"
+                  background="rgba(0, 0, 0, 0.12)"
+                  borderRadius="4px"
+                  padding="8px 22px"
+                  boxShadow="0px 3px 1px -2px rgba(0, 0, 0, 0.20), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12)"
+                  right={590}
+                  fontSize={"15px"}
+                  fontFamily="Roboto"
+                  fontStyle={"normal"}
+                  lineHeight={"20px"}
+                >
+                  CANCELAR
+                </Button>
+              </Flex>
           </ModalBody>
         </ModalContent>
       </Modal>
