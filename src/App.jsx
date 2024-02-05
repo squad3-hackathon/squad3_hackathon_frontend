2f31c6c48b789d6861a3a6d12d2a8ae506f60e3b
jvittor <seu@email.com>
2024-02-05 11:04:17 -0300

    :bug: adicionando import's ao App.jsx e update do Package

--- src/App.jsx
+++ src/App.jsx
@@ -10,23 +10,29 @@ import HomeDiscover from "./Components/Home/HomeDescobrir";
 import EditPerfil from "./Components/Home/EditPerfil";
 import NotFound from "./Components/NotFound";
 import HomeInicio from "./Components/Home/HomeInicio";
+//import LoginTest from "./Components/Home/TestAuth";
+//import Test from "./Components/Home/meu";
+import { AuthProvider } from './Hooks/AuthContext';
 
 const App = () => {
   return (
     <ChakraProvider>
-      <UserProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="home/*" element={<Home />} />
-            <Route path="discover/*" element={<HomeDiscover />} />
-            <Route path="register/*" element={<Register />} />
-            <Route path="profile/*" element={<EditPerfil />} />
-            <Route path="*" element={<NotFound />} />
-            <Route path="/home" element={<HomeInicio />} />
-          </Routes>
-        </BrowserRouter>
-      </UserProvider>
+      <AuthProvider>
+        <UserProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="home/*" element={<Home />} />
+              <Route path="discover/*" element={<HomeDiscover />} />
+              <Route path="register/*" element={<Register />} />
+              <Route path="profile/*" element={<EditPerfil />} />
+              {/*<Route path="test/*" element={<LoginTest />} />*/}
+              <Route path="*" element={<NotFound />} />
+              <Route path="/home" element={<HomeInicio />} />
+            </Routes>
+          </BrowserRouter>
+        </UserProvider>
+      </AuthProvider>
     </ChakraProvider>
   );
 };
